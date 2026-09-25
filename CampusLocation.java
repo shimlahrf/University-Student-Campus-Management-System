dce9b1dee4d11ed7edd9874b420f9b751b39ec7b
23da2-0980-sys <23da2-0980@sltc.ac.lk>
2026-09-25 21:02:57 +0530

    Create CampusLocation.java

---
+++ CampusLocation.java
@@ -0,0 +1,12 @@
+public class CampusLocation {
+
+    private String name;
+
+    public CampusLocation(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
